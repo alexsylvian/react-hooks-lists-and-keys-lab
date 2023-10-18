9dc2834587653e0ae6e3e0ad207e17979d76bdce
alexsylvian <lazeratus1@gmail.com>
2023-10-17 22:01:18 -0400

    list and props test passed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,14 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+  const projectComponents = projects.map((project) => {
+    return <ProjectItem technologies={project.technologies} about={project.about} name={project.name} key={project.id}/>
+  })
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectComponents}</div>
     </div>
   );
 }
